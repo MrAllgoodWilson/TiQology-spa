e119099e147369ef899bd2bdfb55d876e9f137e9
MrAllgoodWilson <alwilson.abc@gmail.com>
2025-11-16 19:26:14 -0500

    feat: add Referral Center module with mock affiliate data

---
+++ src/pages/ReferralCenterPage.tsx
@@ -0,0 +1,13 @@
+// Add your component code here
+
+import React from 'react';
+
+const ReferralCenterPage = () => {
+  return (
+    <div>
+      <h1>Referral Center</h1>
+    </div>
+  );
+};
+
+export default ReferralCenterPage;
